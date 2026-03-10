7eed1dd6b65de9e55dbfad28ce1f2e7acd1de408
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-10 20:25:44 +0300

    Update tickets.js

--- tickets.js
+++ tickets.js
@@ -1,10 +1,16 @@
+// بيانات التكتات
 window.ticketsData = [
   {
-    "timestamp": "2026-03-10T15:40:22.236Z",
+    "timestamp": "2026-03-10T17:23:40.246Z",
     "ticketOwner": "<@1350531070222794804>",
     "ticketOwnerId": "1350531070222794804",
-    "ticketName": "mc・claimed-46",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcclaimed-46.html",
-"users": ["<@7221963986357453312>", "<@1350531070222794804>"]  }
+    "ticketName": "dc・claimed-32",
+    "panelName": "Dc Support",
+    "transcriptFile": "transcripts/transcript-dcclaimed-32.html",
+    "users": [
+      "<@722196398635745312>",
+      "<@1350531070222794804>",
+      "<@1420945820991356990>"
+    ]
+  }
 ];
