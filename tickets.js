b805f3a53110116d13bb267f31dd380677a98011
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-11 00:59:13 +0300

    Update tickets.js - New ticket added

--- tickets.js
+++ tickets.js
@@ -1,132 +1,15 @@
 // بيانات التكتات
 window.ticketsData = [
   {
-    "timestamp": "2026-03-10T20:22:38.165Z",
-    "ticketOwner": "<@1256602196754173993>",
-    "ticketOwnerId": "1256602196754173993",
-    "ticketName": "team・claimed-31",
-    "panelName": "Team Application",
-    "transcriptFile": "transcripts/transcript-teamclaimed-31.html",
-    "users": [
-      "<@1256602196754173993>",
-      "<@939472501359132673>",
-      "<@479258844749496322>",
-      "<@722196398635745312>",
-      "<@282859044593598464>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T19:35:10.227Z",
-    "ticketOwner": "<@1256602196754173993>",
-    "ticketOwnerId": "1256602196754173993",
-    "ticketName": "mc・claimed-55",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcclaimed-55.html",
-    "users": [
-      "<@1256602196754173993>",
-      "<@1420945820991356990>",
-      "<@722196398635745312>",
-      "<@1190305586710073427>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T19:34:20.010Z",
+    "timestamp": "2026-03-10T21:59:11.555Z",
     "ticketOwner": "<@1350531070222794804>",
     "ticketOwnerId": "1350531070222794804",
-    "ticketName": "team・claimed-30",
-    "panelName": "Team Application",
-    "transcriptFile": "transcripts/transcript-teamclaimed-30.html",
-    "users": [
-      "<@722196398635745312>",
-      "<@1350531070222794804>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T19:08:06.817Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "dc・claimed-34",
+    "ticketName": "dc・waiting-35",
     "panelName": "Dc Support",
-    "transcriptFile": "transcripts/transcript-dcclaimed-34.html",
-    "users": [
-      "<@722196398635745312>",
-      "<@1450495899666087948>",
-      "<@1350531070222794804>",
-      "<@1190305586710073427>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T19:00:17.455Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "mc・waiting-54",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcwaiting-54.html",
+    "transcriptFile": "transcripts/transcript-dcwaiting-35.html",
     "users": [
       "<@722196398635745312>",
       "<@1350531070222794804>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T18:24:04.728Z",
-    "ticketOwner": "<@1338485684243136586>",
-    "ticketOwnerId": "1338485684243136586",
-    "ticketName": "whitelist・claimed-32",
-    "panelName": "Wihitelist Application",
-    "transcriptFile": "transcripts/transcript-whitelistclaimed-32.html",
-    "users": [
-      "<@939472501359132673>",
-      "<@722196398635745312>",
-      "<@1190305586710073427>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T18:22:05.581Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "mc・waiting-53",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcwaiting-53.html",
-    "users": [
-      "<@1350531070222794804>",
-      "<@722196398635745312>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T18:06:37.076Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "mc・waiting-52",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcwaiting-52.html",
-    "users": [
-      "<@722196398635745312>",
-      "<@1350531070222794804>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T17:39:01.491Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "mc・waiting-51",
-    "panelName": "Mc Support",
-    "transcriptFile": "transcripts/transcript-mcwaiting-51.html",
-    "users": [
-      "<@722196398635745312>",
-      "<@1350531070222794804>"
-    ]
-  },
-  {
-    "timestamp": "2026-03-10T17:23:40.246Z",
-    "ticketOwner": "<@1350531070222794804>",
-    "ticketOwnerId": "1350531070222794804",
-    "ticketName": "dc・claimed-32",
-    "panelName": "Dc Support",
-    "transcriptFile": "transcripts/transcript-dcclaimed-32.html",
-    "users": [
-      "<@722196398635745312>",
-      "<@1350531070222794804>",
-      "<@1420945820991356990>"
     ]
   }
 ];
