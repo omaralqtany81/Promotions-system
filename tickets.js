36ea880e42fcb6c99a1bd3adda98cbc21a122c06
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-10 21:22:06 +0300

    Update tickets.js - New ticket added

--- tickets.js
+++ tickets.js
@@ -1,4 +1,41 @@
+// بيانات التكتات
 window.ticketsData = [
+  {
+    "timestamp": "2026-03-10T18:22:05.581Z",
+    "ticketOwner": "<@1350531070222794804>",
+    "ticketOwnerId": "1350531070222794804",
+    "ticketName": "mc・waiting-53",
+    "panelName": "Mc Support",
+    "transcriptFile": "transcripts/transcript-mcwaiting-53.html",
+    "users": [
+      "<@1350531070222794804>",
+      "<@722196398635745312>"
+    ]
+  },
+  {
+    "timestamp": "2026-03-10T18:06:37.076Z",
+    "ticketOwner": "<@1350531070222794804>",
+    "ticketOwnerId": "1350531070222794804",
+    "ticketName": "mc・waiting-52",
+    "panelName": "Mc Support",
+    "transcriptFile": "transcripts/transcript-mcwaiting-52.html",
+    "users": [
+      "<@722196398635745312>",
+      "<@1350531070222794804>"
+    ]
+  },
+  {
+    "timestamp": "2026-03-10T17:39:01.491Z",
+    "ticketOwner": "<@1350531070222794804>",
+    "ticketOwnerId": "1350531070222794804",
+    "ticketName": "mc・waiting-51",
+    "panelName": "Mc Support",
+    "transcriptFile": "transcripts/transcript-mcwaiting-51.html",
+    "users": [
+      "<@722196398635745312>",
+      "<@1350531070222794804>"
+    ]
+  },
   {
     "timestamp": "2026-03-10T17:23:40.246Z",
     "ticketOwner": "<@1350531070222794804>",
