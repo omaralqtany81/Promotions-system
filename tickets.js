f1d076430cf9901944251fe70c9fe2914df4f115
omaralqtany81 <omaralqtany81@gmail.com>
2026-03-11 02:25:52 +0300

    Update tickets.js - New ticket added

--- tickets.js
+++ tickets.js
@@ -1,6 +1,20 @@
 // بيانات التكتات
 window.ticketsData = [
   {
+    "timestamp": "2026-03-10T23:25:50.063Z",
+    "ticketOwner": "<@1461469195207311361>",
+    "ticketOwnerId": "1461469195207311361",
+    "ticketName": "whitelist・claimed-34",
+    "panelName": "Wihitelist Application",
+    "transcriptFile": "transcripts/transcript-whitelistclaimed-34.html",
+    "users": [
+      "<@722196398635745312>",
+      "<@999714198240112650>",
+      "<@1461469195207311361>",
+      "<@282859044593598464>"
+    ]
+  },
+  {
     "timestamp": "2026-03-10T21:59:11.555Z",
     "ticketOwner": "<@1350531070222794804>",
     "ticketOwnerId": "1350531070222794804",
